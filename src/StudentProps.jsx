7abc4991d2cc69db3961678a3370c73ecafd41f8
Emmanuel-EO1 <olaleyetolu2003@gmail.com>
2025-08-01 14:32:18 +0100

    Add files via upload

---
+++ src/StudentProps.jsx
@@ -0,0 +1,24 @@
+// props: read-only properties that are shared between components. A parent component can send 
+//         data to a child component. <component key=value />
+// import ProTypes from './src/prop-types'
+function Student(props) {
+    return(
+        <div className="student">
+            <p>Name: {props.name}</p>
+            <p>Age: {props.age}</p>
+            <p>Student: {props.isStudent ? 'Yes' : 'No'}</p>  
+        </div>
+    );
+}
+// Student.proType = {
+//     name: PropTypes.string,
+//     age: PropTypes.number,
+//     isStudent: PropTypes.bool,
+// }
+Student.defaultProps = {
+    name: "Guest",
+    age: 0,
+    isStudent: false,
+}
+
+export default Student
